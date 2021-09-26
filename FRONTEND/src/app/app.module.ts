6fc2228bc6a098cea6a24f4d5424038fc20176f4
Louis Friedrich <louisf.28@hotmail.fr>
2021-09-26 21:46:42 +0200

    fix(): remove unnecessary components

--- FRONTEND/src/app/app.module.ts
+++ FRONTEND/src/app/app.module.ts
@@ -4,27 +4,22 @@ import { FormsModule } from '@angular/forms';
 // import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
 import { FooterComponent } from './footer/footer.component';
 import { AccountFormComponent } from './account-form/account-form.component';
 import { AccountFormSummaryComponent } from './account-form-summary/account-form-summary.component';
-import { AddressComponent } from './address/address.component';
 import { TetiereComponent } from './tetiere/tetiere.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
     FooterComponent,
     AccountFormComponent,
     AccountFormSummaryComponent,
-    AddressComponent,
     TetiereComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    // NgbModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
